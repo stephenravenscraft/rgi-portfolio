19a5f8c7b94415690ddb19bae8b117222c0b98d0
Stephen Ravenscraft <ravengraphics@sbcglobal.net>
2020-03-22 19:56:39 -0500

    Added the grid - varied columns to rgi portfolio

---
+++ deploy/js/sliding-menu.js
@@ -0,0 +1,110 @@
+// accesibility for sliding nav
+
+var MS = {};
+MS.App = (function() {
+  var ESCAPE_CODE = 27;
+  var navButton = $('#menu-button-mobile'),
+      navMenu = $('#side-nav');
+  var navLinks = navMenu.find('a');
+
+  function initApp() {
+    navMenu.on('keydown', handleKeydown);
+    navButton.on('click', handleClick);
+    disableNavLinks();
+  }
+  function handleKeydown(event) {
+    if (event.keyCode === ESCAPE_CODE) {
+      document.body.classList.toggle('active');
+      disableNavLinks();
+      navButton.toggleClass('open');
+      navButton.focus();
+    }
+  }
+  function handleClick(event) {
+    if (document.body.classList.contains('active')) {
+      document.body.classList.remove('active');
+      disableNavLinks();
+    }
+    else {
+      document.body.classList.add('active');
+      enableNavLinks();
+      navLinks.eq(0).focus();
+    }
+  }
+  function enableNavLinks() {
+    navButton.attr('aria-label', 'Menu expanded');
+    navMenu.removeAttr('aria-hidden');
+    navLinks.removeAttr('tabIndex');
+  }
+  function disableNavLinks() {
+    navButton.attr('aria-label', 'Menu collapsed');
+    navMenu.attr('aria-hidden', 'true');
+    navLinks.attr('tabIndex', '-1');
+  }
+  return {
+    init: function(){
+      initApp();
+    }
+  }
+})();
+
+$(document).ready(function($) {
+  new MS.App.init();
+});
+
+//animation for mobile menu button
+
+$(document).ready(function(){
+	$('#menu-button-mobile').click(function(){
+		$(this).toggleClass('open');
+//   $('#side-nav-search-wrapper input').focus();
+	});
+});
+
+//open and close search
+
+$(document).ready(function(){
+	$('#search-button').click(function(){
+		$('#search-overlay').toggleClass('open');
+    $('#search-overlay input').focus();
+	});
+});
+
+$(document).ready(function(){
+	$('#search-close').click(function(){
+		$('#search-overlay').toggleClass('open');
+	});
+});
+
+$(document).keydown(function(e){
+  if(e.keyCode == 27) {
+    if ($('#search-overlay').hasClass('open')) {
+      $('#search-overlay').removeClass('open');
+      $('#search-button').focus();
+    }
+  }
+});
+
+//calculates height of header - adds main-content-wrapper margin-top
+
+$(document).ready(function(){
+  var height = $("#luc-header").height();
+	$("#main-content-wrapper").css("margin-top",height);
+});
+
+$(window).resize(function(){
+  var height = $("#luc-header").height();
+	$("#main-content-wrapper").css("margin-top",height);
+});
+
+$(document).ready(function(){
+  var height = $("#luc-header").height();
+	$("#main-content-wrapper-full-width").css("margin-top",height);
+});
+
+$(window).resize(function(){
+  var height = $("#luc-header").height();
+	$("#main-content-wrapper-full-width").css("margin-top",height);
+});
+
+//END
